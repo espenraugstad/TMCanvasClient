65ff7bc2bae78a31cccafb6fe2eeec09d2e5ec1b
espenraugstad <espen.raugstad@gmail.com>
2026-07-01 21:22:35 +0200

    Added methods to retrieve all modules with items and page content

--- dist/tm-canvas-client.js
+++ dist/tm-canvas-client.js
@@ -7,7 +7,7 @@ class TMCanvasClient {
         console.log("Welcome to the TM Canvas Client");
     }
 
-    async getModules(url = '', modules = []) {
+    async listModules(url = '', modules = []) {
         try {
             if (url === '') {
                 url = `${this.BASE_URL}/courses/${this.COURSE_ID}/modules`
@@ -17,12 +17,83 @@ class TMCanvasClient {
             modules = modules.concat(data);
             const nextLink = this.#findNextLink(res);
             if (nextLink) {
-                return await this.getModules(nextLink, modules);
+                return await this.listModules(nextLink, modules);
             } else {
                 return modules;
             }
         } catch (err) {
-            console.error("Unable to get modules", err);
+            console.error("Unable to list modules", err);
+        }
+    }
+
+    async listPages(url = '', pages = []) {
+        try {
+            if (url === '') {
+                url = `${this.BASE_URL}/courses/${this.COURSE_ID}/pages`
+            }
+            const res = await fetch(url);
+            const data = await res.json();
+            pages = pages.concat(data);
+            const nextLink = this.#findNextLink(res);
+            if (nextLink) {
+                return await this.listPages(nextLink, pages);
+            } else {
+                return pages;
+            }
+
+        } catch (err) {
+            console.error("Unable to list pages", err);
+
+        }
+    }
+
+    async listModulesWithItemsAndPageContent() {
+        const modules = await this.listModules();
+
+        for (const module of modules) {
+            const items = await this.listModuleItems(module.items_url);
+
+            for (const item of items) {
+                if (item.type === "Page") {
+                    const page = await this.getPage(item.url);
+                    if (page) {
+                        item._page_info = page;
+                    } else {
+                        item._page_info = {};
+                    }
+                }
+            }
+            module._module_items = items;
+
+        }
+
+        return modules;
+
+    }
+
+    async getPage(url) {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            return data;
+        } catch (err) {
+            console.error("Unable to get page", err);
+        }
+    }
+
+    async listModuleItems(url, items = []) {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            items = items.concat(data);
+            const nextLink = this.#findNextLink(res);
+            if (nextLink) {
+                return await this.listModuleItems(nextLink, items);
+            } else {
+                return items;
+            }
+        } catch (err) {
+            console.error("Unable to list module items", err);
         }
     }
 
